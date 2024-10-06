fefa1d6bf1f625dd9b9952747d89513c272c5da7
Tom Tramita <tramita.tom@gmail.com>
2024-10-06 07:06:44 -0400

    RestaurantMenu

---
+++ restaurantMenu/restaurant_menu.js
@@ -0,0 +1,15 @@
+const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
+const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+
+const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join(''); //creates content for brekfast menu
+    document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML; //place the content into HTML
+
+
+let mainCourseItem = '';
+mainCourseMenu.forEach((item, index) => {mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});//creates content for maincourse menu
+    document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem; //place the content into HTML
+
+let dessertItem = '';
+for (let i = 0; i < dessertMenu.length; i++) {dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;}
+    document.getElementById('dessertMenuItems').innerHTML = dessertItem;
